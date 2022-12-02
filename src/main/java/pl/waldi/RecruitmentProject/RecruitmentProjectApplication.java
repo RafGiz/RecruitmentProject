eadb7e5157d3931173ebcc3fcae308546163dd67
rafgi <rafgizinski@gmail.com>
2022-12-02 19:39:21 +0100

    first commit

---
+++ src/main/java/pl/waldi/RecruitmentProject/RecruitmentProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.waldi.RecruitmentProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/waldi/RecruitmentProject/RecruitmentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.waldi.RecruitmentProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
